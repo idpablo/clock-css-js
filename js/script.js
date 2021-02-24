b405ab54bf7e7b9bfd88d3199da09e126a847948
Pablo Bento Soares <pablo.soares.dev@gmail.com>
2021-02-24 12:04:26 -0300

    Start JS archive
    The intention is to make it as simple as possible.

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$document).ready(function){
+
+    setInterval(function(){
+
+        var d = new Date();
+        var co = 360/60;
+        var graus_seconds = co * d.getSeconds() + (co + 15);
+
+}
