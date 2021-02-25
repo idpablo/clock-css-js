8e4cc40c7051e87baf1c8a499f49c315412d7d76
Pablo Bento Soares <pablo.soares.dev@gmail.com>
2021-02-25 20:54:05 -0300

    Fail js implementarion

--- js/script.js
+++ js/script.js
@@ -1,25 +1,29 @@
-   setInterval(function()
+$(document).ready(function(){  
+    
+    setInterval(function()
     {
 
         var d = new Date();
         var co = 360/60;
-        var graus_seconds = co * d.getSeconds() + (co + 15);
+        var graus_seconds = co * d.getSeconds() + (co * 15);
 
-        $(".pointer-seconds").css({
+        $(".pinter-seconds-move").css({
             "transform": "rotate(" +graus_seconds+ "deg)"
           });
 
         var co = 360 / 60;
         var graus_seconds = co * (m = d.getMinutes()) + (co * 15);
 
-        $(".pointer-minutes").css({
+        $(".pointer-minutes-move").css({
             "transform": "rotate(" +graus_seconds+"deg)"
         });
 
         var co = 360 / 12;
         var graus_seconds = co * (h = d.getHours()) + (co * 3);
 
-        $(".pointer-hours").css({
+        $(".pointer-hours-move").css({
             "transform": "rotate(" +graus_seconds+"deg)"
         });
     }, 500);
+
+});
