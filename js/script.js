dce337996404ad6c9c0d7f0b6c1c1919c86b3f23
Pablo Bento Soares <pablo.soares.dev@gmail.com>
2021-02-26 06:01:34 -0300

    up

--- js/script.js
+++ js/script.js
@@ -1,29 +1,29 @@
-$(document).ready(function(){  
-    
-    setInterval(function()
-    {
+import {$} from "jQuery";
+
+$(document).ready(function () {
+
+    setInterval(function () {
 
         var d = new Date();
-        var co = 360/60;
-        var graus_seconds = co * d.getSeconds() + (co * 15);
+        var co = 360 / 60;
+        var graus_seconds = (co * d.getSeconds()) + (co * 15);
 
-        $(".pointer-seconds-move").css({
-            "transform": "rotate(" +graus_seconds+ "deg)"
-          });
+        $(".pointer-seconds").css({
+            "transform": "rotate(" + graus_seconds + "deg)"
+        });
 
         var co = 360 / 60;
-        var graus_seconds = co * (m = d.getMinutes()) + (co * 15);
+        var graus_seconds = (co * (m = d.getMinutes()) + (co * 15));
 
-        $(".pointer-minutes-move").css({
-            "transform": "rotate(" +graus_seconds+"deg)"
+        $(".pointer-minutes").css({
+            "transform": "rotate(" + graus_seconds + "deg)"
         });
 
         var co = 360 / 12;
         var graus_seconds = co * (h = d.getHours()) + (co * 3);
 
-        $(".pointer-hours-move").css({
-            "transform": "rotate(" +graus_seconds+"deg)"
+        $(".pointer-hours").css({
+            "transform": "rotate(" + graus_seconds + "deg)"
         });
     }, 500);
-
 });
