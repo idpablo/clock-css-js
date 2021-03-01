70c0afd874865e827084b75ac4fb15a4fca0a42e
Pablo Bento Soares <pablo.soares.dev@gmail.com>
2021-03-01 11:53:50 -0300

    Finish clock css and js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
-import {$,jQuery} from 'jquery';
-
-$(document).ready(function () {
-
     setInterval(function () {
 
+        //Variables declaration.
+        //Capture Date of System.
         var d = new Date();
         var co = 360 / 60;
         var graus_seconds = (co * d.getSeconds()) + (co * 15);
 
+        //converter seconds in graus 
+        //set 15 graus angular 1 second
         $(".pointer-seconds").css({
             "transform": "rotate(" + graus_seconds + "deg)"
         });
@@ -26,4 +26,3 @@ $(document).ready(function () {
             "transform": "rotate(" + graus_seconds + "deg)"
         });
     }, 500);
-});
